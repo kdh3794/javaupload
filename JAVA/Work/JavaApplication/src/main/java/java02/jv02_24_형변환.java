80baf4225f7529765a4b90cb96b2acac7a118661
kdh3794 <kdh3794@nate.com>
2017-09-18 15:33:10 +0900

    dd

--- JAVA/Work/JavaApplication/src/main/java/java02/jv02_24_형변환.java
+++ JAVA/Work/JavaApplication/src/main/java/java02/jv02_24_형변환.java
@@ -8,18 +8,18 @@ public class jv02_24_형변환 {
         result= 1 + '2';
         System.out.println("result ==" +result);
         
-        result= '2' + true;
-        String true = String.valueOf(result);
+        result= '2' + ((true ? 1:0));
+        
         System.out.println("result ==" +result);
  
         
         System.out.println("result ==" +result);
         
-        result= "15" - 5;
+        result= Integer.valueOf("15") - 5;
         System.out.println("result ==" +result);
         
-        result= "1" + '2';
-        System.out.println("result ==" +result);
+       
+        result= Integer.valueOf("1") + '2';
         
         result= false + true; 
         System.out.println("result ==" +result);
