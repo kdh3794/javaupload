dd4e1d5be6266c3fe467836a0d421a3b74528ae1
kdh3794 <kdh3794@nate.com>
2017-11-17 14:37:13 +0900

    gg

--- JAVA/Work/JavaApplication/src/main/java/honja2/PhoneInfo.java
+++ JAVA/Work/JavaApplication/src/main/java/honja2/PhoneInfo.java
@@ -1,130 +1,145 @@
 package honja2;
 
-import java.util.Scanner;
+import java.util.*;
+
 
 class PhoneBookManager{
-    //데이터의 저장, 검색, 삭제
-    PhoneInfo [] phoneBook;
-    private int count; //전화번호부에 저장되있는 사람 수.
-    Scanner sc = new Scanner(System.in);
-    
-    public PhoneBookManager(){
-        phoneBook=new PhoneInfo[100];
-        count=0;
-    }
-    //데이터의 검색
-    public void serachBook(){
-        
-        int index;
-        System.out.print("찾을 이름 입력 : ");
-        String name=sc.nextLine();
-        index=search(name);
-        if(index<0)
-            System.out.println("해당 하는 이름이 없습니다 ");
-        else{
-            phoneBook[index].showInfo();
-            System.out.println("데이터 검색 완료 \n");
-        }
-    }
-    public int search(String name){
-        //이름 찾기
-        for(int index=0; index<count; index++){
-            if(name.compareTo(phoneBook[index].getName()) == 0){
-                return index;
-            }
-        }
-        return -1;
-        
-    }
-    //데이터의 삭제
-    public void removeBook(){
-        
-        int index;
-        System.out.print("삭제할 이름 입력 : ");
-        String name = MenuViewer.sc.nextLine();
-        index=search(name);
-        if(index<0)
-            System.out.println("해당하는 값이 없습니다 . ");
-        else{
-            for(int i = index; i<count-1 ; i++){
-                phoneBook[i]=phoneBook[i+1];
-                
-            }
-            count--;
-            System.out.println("데이터 삭제 완료 \n ");
-        }
-    }
-    //데이터의 저장
-    public PhoneInfo readPhoneBook(){ //일반
-        System.out.print("이름:");
-        
-    }
-    
-    
+ //데이터의 저장, 검색, 삭제
+ 
+ PhoneInfo [] phoneBook;
+ private int count;//전화번호부에 저장되잇는 사람수
+ Scanner keyboard = new Scanner(System.in);
+ 
+ public PhoneBookManager(){
+  phoneBook=new PhoneInfo[100];
+  count=0;  
+ }
+ //데이터의 검색
+ public void searchBook(){
+  
+  int index;
+  System.out.print("찾을 이름 입력:");
+  String name=keyboard.nextLine();
+  index=search(name);
+  if(index<0)
+   System.out.println("해당하는 값이 없습니다.");
+  else{
+   phoneBook[index].showInfo();
+   System.out.println("데이터검색 완료\n");
+  }
+ }
+ 
+ public int search(String name){//이름찾기
+  
+  for(int index=0;index<count;index++){
+   if(name.compareTo(phoneBook[index].getName())==0){
+    return index;
+   }
+  }
+  return -1;
+ }
+ //데이터의 삭제
+ public void removeBook(){
+
+  int index;
+  System.out.print("삭제할 이름 입력:");
+  String name=keyboard.nextLine();
+  index=search(name);
+  if(index<0)
+   System.out.println("해당하는 값이 없습니다.");
+  else{
+   for(int i=index;i<count-1;i++){
+    phoneBook[i]=phoneBook[i+1];    
+   }
+   count--;
+   System.out.println("데이터삭제 완료\n");
+  }
+   
+ }
+ //데이터의 저장
+ public void readBook(){//입력처리
+  System.out.print("이름:");
+  String name = keyboard.nextLine();
+  System.out.print("전화번호:");
+  String phoneNumber = keyboard.nextLine();
+  System.out.print("생년월일:");
+  String birthday = keyboard.nextLine();
+  phoneBook[count++] = new PhoneInfo(name,phoneNumber,birthday);
+ }
+ 
+ 
+}
+
+class PhoneInfo{
+ 
+ private String name;//이름
+ private String phoneNumber;//핸드폰번호
+ private String birthday;//생년월일
+ 
+ public PhoneInfo(String name, String phoneNumber, String birthday){
+  this.name = name;
+  this.phoneNumber = phoneNumber;
+  this.birthday = birthday;
+  
+ }
+ public PhoneInfo(String name, String phoneNumber){
+  this.name = name;
+  this.phoneNumber = phoneNumber;
+  this.birthday = null;
+ }
+ public void showInfo(){
+  System.out.println(name+"\n"+phoneNumber);
+  if(birthday!=null)
+   System.out.println(birthday);
+ 
+  System.out.println();
+  
+ }
+ public String getName(){
+  return name;
+ }
 }
 
 
+class PhoneMain {
 
+ static Scanner keyboard = new Scanner(System.in);
 
-
-
-
-public class PhoneInfo {
-    
-    // 생년 월일 정보는 저장을 할 수도 저장 하지 않을 수도 있게 생성자를 정의하자(오버로딩)
-    private String name;
-    private String phoneNumber;
-    private String birthday;
-    
-    public PhoneInfo(String name, String phoneNumber, String birthday) {
-        
-        this.name = name;
-        this.phoneNumber = phoneNumber;
-        this.birthday = birthday;
-    }
-    
-    public PhoneInfo(String name, String phoneNumber) { // 이쪽에는 왜 birthday 가 없지.
-        // 위에도 그렇고 여기도 super()가 안 들어감.
-        this.name = name;
-        this.phoneNumber = phoneNumber;
-        this.birthday = null;
-    }
-    
-    public void showInfo() {
-        System.out.println(name + "\n" + phoneNumber);
-        if (birthday != null)
-            System.out.println(birthday);
-        System.out.println();
-    }
-    
-}
-
-public class PhoneMain {
-    static Scanner sc = new Scanner(System.in);
-    
-    public static PhoneInfo input() {// 입력처리
-        System.out.print("이름 : ");
-        String name = sc.nextLine();
-        System.out.print("전화번호 : ");
-        String phoneNumber = sc.nextLine();
-        System.out.print("생년월일 : ");
-        String birthday = sc.nextLine();
-        return new PhoneInfo(name, phoneNumber, birthday);
-        
-    }
-    
-    public static void main(String args[]) {
-        PhoneInfo phone;
-        for (;;) {
-            System.out.println("1:입력 \n2:종료 ");
-            int input = sc.nextInt();
-            sc.nextLine();
-            if (input == 1)
-                ;
-            else
-                break;
-            phone.showInfo();
-        }
-    }
-    
+ public static void showMenu(){//메뉴를 보여주는화면
+  System.out.println("---------전화번호부관리프로그램ver3----------");
+  System.out.println("선택하세요..");
+  System.out.println("1. 데이터 입력");
+  System.out.println("2. 데이터 검색");
+  System.out.println("3. 데이터 삭제");
+  System.out.println("4. 데이터 종료");
+  
+ }
+ 
+ public static void main(String ar[]){
+ // PhoneInfo phone;
+  PhoneBookManager phoneManager = new PhoneBookManager();
+  
+  while(true){//무한루프
+   
+   showMenu();
+   int input=keyboard.nextInt();
+   keyboard.nextLine();
+  
+   switch(input){
+   case 1://입력
+     phoneManager.readBook();
+     break;
+   case 2://찾기
+     phoneManager.searchBook();
+     break;
+   case 3://삭제
+     phoneManager.removeBook();
+     break;
+   case 4: System.out.println("프로그램을종료합니다.");
+     return;
+   }
+      
+  }
+  
+ }
 }
