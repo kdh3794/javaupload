1a596386d5ad988a49ec585d2be31f845dc26dda
kdh3794 <kdh3794@nate.com>
2017-11-24 15:18:58 +0900

    dddd

--- JAVA/Work/JavaApplication/src/main/java/강동훈/PhoneInfo.java
+++ JAVA/Work/JavaApplication/src/main/java/강동훈/PhoneInfo.java
@@ -2,20 +2,20 @@ package 강동훈;
 
 import java.util.Scanner;
 
-class Phonebook2 {
+class PhoneInfo {
     
     private String name;
     private String phoneNumber;
     private String birthday;
     
-    public Phonebook2(String name, String phoneNumber, String birthday) {
+    public PhoneInfo(String name, String phoneNumber, String birthday) {
         
         this.name = name;
         this.phoneNumber = phoneNumber;
         this.birthday = birthday;
     }
     
-    public Phonebook2() {
+    public PhoneInfo() {
         
     }
     public void showInfo(){
@@ -31,19 +31,21 @@ class Phonebook2 {
 class PhoneMain{
    
  
+    
+
      static Scanner sc = new Scanner(System.in);
      
-     public static Phonebook2 input(){
+     public static PhoneInfo input(){
          System.out.print("이름:");
          String name = sc.nextLine();
          System.out.print("전화번호:");
          String phoneNumber = sc.nextLine();
          System.out.print("생년월일:");
          String birthday = sc.nextLine();
-         return new Phonebook2(name,phoneNumber,birthday);
+         return new PhoneInfo(name,phoneNumber,birthday);
         }
      public static void main(String ar[]){
-         Phonebook2 phone;
+         PhoneInfo phone;
          
          for(;;){
              System.out.println("1:입력 \n 2:종료");
